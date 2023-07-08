e656f25c71d676ba2d34a0a37befc4c829b59ae9
Raphael Kazuhiro Yoshiki <74300967+RaphaelYoshiki@users.noreply.github.com>
2023-07-08 19:51:25 -0300

    Continuando backend

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 import express from "express";
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc, getDocs } from 'firebase/firestore/lite';
+import { getFirestore, collection, addDoc, doc, getDoc, updateDoc, deleteDoc } from 'firebase/firestore/lite';
 
 // Import the functions you need from the SDKs you need
 // TODO: Add SDKs for Firebase products that you want to use
@@ -20,23 +20,137 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+//const app = initializeApp(firebaseConfig);
 //const analytics = getAnalytics(app);
-const db = getFirestore(app);
+//const db = getFirestore(app);
 
-try {
-	const docRef = await addDoc(collection(db, "users"), {
-	  username: "Ada",
-	  nickname: "Lovelace",
-	  password: "1815",
-	  email: "aaaa",
-	});
-	console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-	console.error("Error adding document: ", e);
-}
+const app = express();
+app.use(express.json());
 
-const querySnapshot = await getDocs(collection(db, "users"));
-querySnapshot.forEach((doc) => {
-  console.log(`${doc.id} => ${doc.data()}`);
+const initializeFirebase = () => {
+  const firebaseApp = initializeApp(firebaseConfig);
+  return getFirestore(firebaseApp);
+};
+
+
+//Cadastro
+app.post("/cadastra", async (req, res) => {
+  const db = initializeFirebase();
+
+  try {
+    const { username, password, firstname, lastname, email } = req.body;
+
+    const docRef = await addDoc(collection(db, "users"), {
+      username,
+      password,
+      firstname,
+      lastname,
+      email,
+    });
+
+    console.log("Document written with ID: ", docRef.id);
+    res.status(200).send("OK");
+  } catch (e) {
+    console.error("Error adding document: ", e);
+    res.status(500).send("Error");
+  }
+});
+
+//Login
+app.get("/login/:username/:password", async (req, res) => {
+  const db = initializeFirebase();
+  const { username, password } = req.params;
+
+  const docRef = doc(collection(db, "users"), username);
+  const docSnap = await getDoc(docRef);
+  const user = docSnap.data();
+
+  console.log(username, password);
+
+  if (user && user.password == password) {
+    res.status(200).send("OK");
+  } else {
+    res.status(401).send("Unauthorized");
+  }
+});
+
+
+//Msg em chat vazio
+app.post("/mensagem", async (req, res) => {
+  const db = initializeFirebase();
+  const { chatid, userid, mensagem } = req.body;
+
+  try {
+    const docRef = await getDoc(collection(db, "chats"), chatid);
+    const chat = docRef.data();
+
+    if (!chat) {
+      res.status(404).send("Chat not found");
+    } else {
+      chat.mensagens.push({ userid, mensagem });
+      await updateDoc(docRef.ref, chat);
+      res.status(200).json(chat);
+    }
+  } catch (e) {
+    console.error("Error adding message: ", e);
+    res.status(500).send("Error");
+  }
+});
+
+//Msg em chat existente
+app.get("/chat/:chatid", async (req, res) => {
+  const db = initializeFirebase();
+  const { chatid } = req.params;
+
+  const docRef = await getDoc(collection(db, "chats"), chatid);
+  const chat = docRef.data();
+
+  if (chat) {
+    res.status(200).json(chat);
+  } else {
+    res.status(404).send("Chat not found");
+  }
+});
+
+//Abrir chat
+app.put("/chat/:chatid", async (req, res) => {
+  const db = initializeFirebase();
+  const { chatid } = req.params;
+  const { titulo } = req.body;
+
+  const docRef = await getDoc(collection(db, "chats"), chatid);
+  const chat = docRef.data();
+
+  if (chat) {
+    chat.titulo = titulo;
+    await updateDoc(docRef.ref, chat);
+    res.status(200).json(chat);
+  } else {
+    res.status(404).send("Chat not found");
+  }
+});
+
+//Deletar chat
+app.delete("/chat/:chatid", async (req, res) => {
+  const db = initializeFirebase();
+  const { chatid } = req.params;
+
+  try {
+    const docRef = await getDoc(collection(db, "chats"), chatid);
+    const chat = docRef.data();
+
+    if (chat) {
+      await deleteDoc(docRef.ref);
+      res.status(200).send("OK");
+    } else {
+      res.status(404).send("Chat not found");
+    }
+  } catch (e) {
+    console.error("Error deleting chat: ", e);
+    res.status(500).send("Error");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
 });
