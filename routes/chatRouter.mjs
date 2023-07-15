ce4355f1d16f962a95d092175d8ee99155686472
Gabriel Ribeiro <g2012ribeiro@gmail.com>
2023-07-15 01:27:44 -0300

    Ajustes para o front-end

--- routes/chatRouter.mjs
+++ routes/chatRouter.mjs
@@ -1,115 +1,152 @@
 import express from "express";
-import { query, getDoc, addDoc } from 'firebase/firestore/lite';
+import { query, addDoc, getDocs, where } from 'firebase/firestore/lite';
+import { getMessages, getChats } from "../dao/firebaseConnection.mjs";
 
 const router = express.Router();
 
+//Todos os chats
 router.get("/chats", async (req, res) => {
-	const { userID } = req.body;
-  
+	console.log("GET /chats");
+
 	try{
-	  const chatsRef = query(getChats(), where("userID", "==", userID))
-	  const chatsDoc = await getDocs(chatsRef);
-  
-	  let chats = [];
-  
-	  chatsDoc.forEach((elem) => {
-		const { titulo } = elem.data();
-		chats.push({
-		  chatID: elem.id,
-		  titulo: titulo
-		})
-	  });
-	  console.log("GET /chats")
-	  res.status(200).send(chats);
+		const firebaseChats = await getDocs(getChats());
+	
+		let chats = [];
+	
+		firebaseChats.forEach((elem) => {
+			const { title } = elem.data();
+			chats.push({
+				chatID: elem.id,
+				title: title
+			})
+		});
+			
+		console.log("Realizado com Sucesso");
+		res.status(200).send(chats);
 	}
 	catch (error){
-	  console.log(error);
-	  res.status(500).send(error);
+		console.log(error);
+		res.status(500).send(error);
 	}
 });
 
-router.get("/chats/messages", async (req, res) => {
-	const { chatID } = req.body;
-  
+//chats com mensagens do usuario
+router.get("/chats/:userID", async (req, res) => {
+	const userID = req.params.userID;
+
+	console.log("GET /chats/"+userID);
+
 	try{
-		//Adiciona mensagem do bot aleatoriamente
-		//Busca mensagens do chat e retorna
-		console.log("GET /messages");
-		res.status(200).send({
-			chatID: newChatID,
-			messages: messages
-		});
+		const messagesQuery = query(getMessages(), where("owner", "==", userID))
+		const firebaseMessages = await getDocs(messagesQuery);
+
+		let chats = [];
+		firebaseMessages.forEach((elem) => chats.push(elem.data().chat))
+		chats.filter((elem, index) => chats.indexOf(elem) == index);
+		
+		console.log("Realizado com Sucesso");
+		res.status(200).send(chats);
+	}
+	catch (error){
+		console.log(error);
+		res.status(500).send(error);
+	}
+});
+
+router.get("/messages/", async (req, res) => {
+	console.log("GET /messages/");
+
+	try{
+		console.log("Realizado com Sucesso");
+		res.status(200).send([]);
 	}
 	catch (error) {
-	  console.log(error.message);
-	  res.status(500).send(error.message);
+		console.log(error.message);
+		res.status(500).send(error.message);
 	}
 });
 
-router.post("/chats", async (req, res) => {
-	const { chatID, userID, message, timeStamp } = req.body;
+router.get("/messages/:chatID", async (req, res) => {
+	const chatID = req.params.chatID;
+
+	console.log("GET /messages/"+chatID);
   
 	try{
-	  let newChatID;
-  
-	  if (!chatID) {
-		const docRef = await addDoc(getChats(), {
-		  userID: userID,
-		  titulo: message
-		});
-		newChatID = docRef.id;
-	  }
-	  newChatID = chatID ? chatID : newChatID
-  
-	  const msgDocRef = await addDoc(getMessages(), {
-		chatID: newChatID,
-		message: message,
-		timeStamp: timeStamp,
-		userFlag: true
-	  });
-  
-	  const messagesRef = query(getMessages(), where("chatID", "==", newChatID))
-	  const messagesDoc = await getDocs(messagesRef);
-  
-	  let messages = [];
-  
-	  messagesDoc.forEach(doc => {
-		const {timeStamp, userFlag, message} = doc.data();
-		messages.push({
-		  timeStamp: timeStamp,
-		  userFlag: userFlag,
-		  message: message
+		/*if(Math.random()%20){
+			const docRef = await addDoc(getMessages(), {
+				chat: chatID,
+				owner: undefined,
+				content: randomMessage(),
+				time: Date.now()
+			});
+		}*/
+		let messages = [];
+
+		const messagesQuery = query(getMessages(), where("chat", "==", chatID));
+		const firebaseMessages = await getDocs(messagesQuery);
+
+		firebaseMessages.forEach(elem => {
+			const {time, owner, content} = elem.data();
+			messages.push({
+				time: time,
+				owner: owner,
+				content: content
+			})
 		})
-	  })
+		
+		console.log("Realizado com Sucesso");
+		res.status(200).send(messages);
+	}
+	catch (error) {
+		console.log(error.message);
+		res.status(500).send(error.message);
+	}
+});
+
+//Registra mensagem
+router.post("/messages", async (req, res) => {
+	const { chatID, userID, content, time } = req.body;
+	
+	console.log("POST /messages")
+
+	try{
+		let newChatID;
+	
+		if (!chatID) {
+			const docRef = await addDoc(getChats(), {
+				title: content
+			});
+			newChatID = docRef.id;
+		}
+		newChatID = chatID ? chatID : newChatID
+
+		const msgDocRef = await addDoc(getMessages(), {
+				chat: newChatID,
+				content: content,
+				time: time,
+				owner: userID
+		});
+	
+		const messagesRef = query(getMessages(), where("chatID", "==", newChatID))
+		const messagesDoc = await getDocs(messagesRef);
+	
+		let messages = [];
+	
+		messagesDoc.forEach(elem => {
+			const {time, owner, content} = elem.data();
+			messages.push({
+				time: time,
+				owner: owner,
+				content: content
+			})
+		})
 	  
-	  console.log("POST");
-	  res.status(200).send({
-		chatID: newChatID,
-		messages: messages
-	  });
+		console.log("Realizado com Sucesso");
+	  	res.status(200).send(messages);
 	}
 	catch (error) {
-	  console.log(error.message);
-	  res.status(500).send(error.message);
-	}
-});
-  
-router.put("/chats", async (req, res) => {
-//Atualizar titulo do chat	
-});
-  
-router.delete("/chat", async (req, res) => {
-	const { chatid } = req.params;
-  
-	try {
-	  const chatDoc = await getDoc(getChats(), chatid);
-	  const chat = docRef.data();
-	  //deletar mensagens do chat
-	  //deletar chat
-	}
-	catch (error) {
-	  console.log(error.message);
-	  res.status(500).send(error.message);
+		console.log(error.message);
+		res.status(500).send(error.message);
 	}
 });
 
--- routes/userRouter.mjs
+++ routes/userRouter.mjs
@@ -15,7 +15,7 @@ router.get("/login", async (req, res) => {
 		);
 	  	const userDoc = await getDocs(usersRef);
 	  	const userData = userDoc.docs[0];
-  
+		//retornar nome do usuario também
 	  	res.status(200).send(userData ? userData.id : false);
 	}
 	catch (error){
@@ -26,30 +26,28 @@ router.get("/login", async (req, res) => {
 
 router.post("/cadastra", async (req, res) => {
 	const { username, password, firstname, lastname, email, date } = req.body;
-	//const newUser = new User(firstname, lastname, email, date);
+
 	try {
-		//validar
-		//username ou email ja cadastrado?
 		const usrnmQuery = query(getUsers(), where("username", "==", username));
 		const emailQuery = query(getUsers(), where("email", "==", email));
 		const existingNameDoc = await getDocs(usrnmQuery);
 		const existingEmailDoc = await getDocs(emailQuery);
 		if (existingNameDoc.size > 0) {
 			console.log("Nome de usuário já existe!");
-			res.status(202).send("Non-original username");
+			res.status(202).send("Nome de usuário ja existe!");
 		}
 		else if (existingEmailDoc.size > 0) {
 			console.log("Endereço de email já está em uso!");
-			res.status(202).send("Email in use");
+			res.status(202).send("Endereço de email já está em uso!");
 		}
 		else {
 			const docRef = await addDoc(getUsers(), {
-			username: username,
-			password: password,
-			firstname: firstname,
-			lastname: lastname,
-			email: email,
-			date: date
+				username: username,
+				password: password,
+				firstname: firstname,
+				lastname: lastname,
+				email: email,
+				date: date
 			});
 	
 			res.status(200).send(new User(firstname, lastname, email, date));
